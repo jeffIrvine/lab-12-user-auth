517b67042929c916ae4f71c74d9aa9a1cb3b793f
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-11-10 21:59:36 -0800

    made some routes

---
+++ data/todos.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'wash the dishes',
+    completed: false,
+    owner_id: 2
+  },
+  {
+    id: 2,
+    todo: 'vacuum',
+    completed: false,
+    owner_id: 2
+  },
+  {
+    id: 3,
+    todo: 'take out the trash',
+    completed: false,
+    owner_id: 2
+  },
+  {
+    id: 4,
+    todo: 'take out the trash',
+    completed: false,
+    owner_id: 2
+  }
+];
